fa55909e6d2c01699ab3a9c6f55e7d16436ca046
Felipe Lula Thums <lulafelipe7@gmail.com>
2026-05-10 22:47:53 -0300

    Reduce array size and adjust loops in L10-Ex03.c

--- 07-05/L10-Ex03.c
+++ 07-05/L10-Ex03.c
@@ -1,5 +1,3 @@
-// Online C Compiler - Build, Compile and Run your C programs online in your favorite browser
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -8,21 +6,27 @@ int main()
 {
     
     
-    int vet[20][20];
+    int vet[10][10];
     int numaleatorio;
     int soma = 0;
+    int rec = 0;
     
     srand(time(NULL));
   
-    for (int j = 0; j < 20; j++) {
-        for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < 10; i++) {
             vet[j][i] = rand()  % 89 + 10;
             soma += vet[j][i];
             printf("%d ", vet[j][i]);
+            if(rec <= soma){
+                rec = soma;
+            }
         }
         printf("= %d\n", soma);
         soma = 0;
     }
+    
+    printf("\nO maior é %d", rec);
 
     return 0;
 }
